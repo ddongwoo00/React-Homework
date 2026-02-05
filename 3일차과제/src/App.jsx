020dec3fa705720487c6925ab06d1b9b334135f0
ddongwoo00 <hdw4826@gmail.com>
2026-02-05 13:27:48 +0900

    useState, props 부분 추가

--- 3일차과제/src/App.jsx
+++ 3일차과제/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import CountResult from './CountResult';
-
+import { useState } from 'react';
 function App() {
   // 1.useState를 사용하여 상태를 활용하여 기능을 구현하세요.
   const [count, setCount] = useState(0)
@@ -11,9 +11,9 @@ function App() {
       <p>버튼을 누를 때 마다 버튼이 실시간으로 변경되도록 구현해 주세요.</p>
       <h2>count: {count}</h2>
       <button onClick={() => setCount(count + 1)}>+</button>
-      <button onClick={() => setCount(count - 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
       {/* 2. 상태로 관리하고 있는 count 값을 props를 통해 CountResult 컴포넌트로 전달하세요. */}
-      <CountResult />
+      <CountResult count={count} />
     </>
   );
 }
